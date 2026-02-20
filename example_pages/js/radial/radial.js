6ebb5ae790f0c41b19c6c37d0941228be4910df8
matthew <matthew@wgolden.com>
2026-02-20 08:00:37 -0600

    prevent blur on zoom

--- example_pages/js/radial/radial.js
+++ example_pages/js/radial/radial.js
@@ -16,30 +16,25 @@ export class Radial {
     }
 
     focusOnNode(node) {
-        const d3 = this.d3
-        const svgRoot = d3.select(this.chart).select("svg")
+    const d3 = this.d3
+    const svg = d3.select(this.chart).select("svg")
 
-        const cx = node.y * Math.cos(node.x - Math.PI/2)
-        const cy = node.y * Math.sin(node.x - Math.PI/2)
+    const cx = node.y * Math.cos(node.x - Math.PI/2)
+    const cy = node.y * Math.sin(node.x - Math.PI/2)
 
-        const width = +svgRoot.attr("width")
-        const height = +svgRoot.attr("height")
+    const scale = this.scale || 1
 
-        const scale = this.scale || 1
+    const newTransform = d3.zoomIdentity
+        .translate(
+            this.chartRadius - cx * scale,
+            this.chartRadius - cy * scale
+        )
+        .scale(scale)
 
-        const tx = width/2 - cx * scale
-        const ty = height/2 - cy * scale
-
-        const newTransform = d3.zoomIdentity
-            .translate(tx, ty)
-            .scale(scale)
-
-        svgRoot.transition()
-            .duration(750)
-            .call(this.zoom.transform, newTransform)
+    svg.transition()
+        .duration(750)
+        .call(this.zoom.transform, newTransform)
     }
-
-
 
     render(root) {
 
@@ -54,28 +49,28 @@ export class Radial {
         // clear previous chart
         chart.selectAll("svg").remove()
 
-        const svgRoot = chart
+        const svg = chart
             .append("svg")
                 .attr("width", self.chartWidth)
                 .attr("height", self.chartWidth)
+                .attr("id", "radial-tree")
                 
 
-        const svg = svgRoot
-            .append("g")
-                .attr("id", "radial-tree")
+        const zoomContainer = svg.append("g")
+                
 
         self.zoom = d3.zoom()
             .scaleExtent([0.5, 5])
             .on("zoom", (event) => {
                 self.scale = event.transform.k
-                svg.attr("transform", event.transform)
+                zoomContainer.attr("transform", event.transform)
             })
 
         // attach zoom to the SVG
-        svgRoot.call(self.zoom)
+        svg.call(self.zoom)
 
         // set initial transform using same zoom instance
-        svgRoot.call(
+        svg.call(
             self.zoom.transform,
             d3.zoomIdentity.translate(self.chartRadius, self.chartRadius).scale(1)
         )
@@ -85,19 +80,21 @@ export class Radial {
         tree(hierarchy)
 
         // links
-        svg.append("g")
+        zoomContainer.append("g")
                 .attr("id", "radial-links")
             .selectAll("path")
             .data(hierarchy.links())
             .enter()
             .append("path")
+            .attr("vector-effect", "non-scaling-stroke")
+            .attr("shape-rendering", "geometricPrecision")
             .attr("class", "link")
             .attr("d", d3.linkRadial()
                 .angle(d => d.x)
                 .radius(d => d.y))
 
         //nodes
-        const node = svg.append("g")
+        const node = zoomContainer.append("g")
                 .attr("id", "radial-nodes")
             .selectAll("g")
             .data(hierarchy.descendants())
